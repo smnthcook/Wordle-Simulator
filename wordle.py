29c6c7c33a71a0b140cb083f8474c1f9f6d07c10
Samantha Cook <71744658+smnthcook@users.noreply.github.com>
2023-05-03 13:46:08 -0500

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -4,37 +4,62 @@ Samantha Cook
 4/28/2023
 '''
 
-import random, colorama, Back, init
+import random
+import colorama
+import os
+from colorama import Fore, Back
 
 
-def play_round():
-    pass
+def play_game():
+    guess = '' 
+    word = select_word()
+    attempts = 6 
+    while attempts > 0 and guess != word:
+        guess = input('Enter a guess >> ' ).lower()
+        if check_word(guess, word):
+            attempts(guess,word)
+            attempts -= 1
+        else:
+            print('That is an invaild word. Enter a different word. >> ')
+            if guess != word:
+                print('Out of guesses. That answer was', word.strip())
 
-colorama.init()
 
 def select_word():
-    choice_list = []
+    word_list = []
     file = open('wordle_words.txt', )
-    choice_list = file.readlines()
-    choice = random.choice(choice_list)
-    return choice
+    word_list = file.readlines()
+    word = random.choice(word_list)
+    print(word.strip())
+    return word, file
 
 
-def check_word():
+def check_word(guess, word):
+    if len(guess) != 5:
+        return False
+    file = open('wordle_words.txt', 'r')
+    for word in file:
+        word.append(word.strip())
+    file.close
+    if guess in word:
+        return True
+    return False 
+            
+def check_letter():
     choice = select_word()
     attempt = 6
     while attempt > 0:
+        print()
         guess = str(input("Enter your guess: "))
-        if guess[0] == choice[0]:
-            print(choice[0])
-            break
-        elif guess[1] == choice[1]:
-            print(choice[1])
-        else: 
-            attempt -= 1
-def check_letter():
-    pass
+        
+        for i in range(len(choice) - 1):
+            print(Back.GREEN + choice[i], end='' + Back.RESET)
+        attempt = attempt - 1
 
 
-
-check_word()
+#main
+print('Welcome to Wordle Simulator!')
+repeat = 'y'
+while repeat == 'y':
+    play_game()
+    repeat = input('Would you like to play again? (y/n) >> ')
